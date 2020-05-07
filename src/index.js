9cc73c1f919b98f209e355bd9d48a0888053688a
Michael Olatunji <tundexmike@gmail.com>
2020-05-07 17:27:21 +0100

    eslint bug fixed

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@
 const digitsOnly = /^\d+$/;
 
 // function to mask digits into US phone format
-export function maskNIGPhone(phone) {
-    // returning null when getting null, undefined, or an object that is no string as a parameter
-    if (!phone || typeof phone !== 'string') {
-        return null;
-    }
+function maskNIGPhone(phone) {
+  // returning null when getting null, undefined, or an object that is no string as a parameter
+  if (!phone || typeof phone !== 'string') {
+    return null;
+  }
 
-    // returning the untouched value when it contains non-digit chars or when it has a length != 10
-    if (digitsOnly.test(phone) === false || phone.length !== 10) {
-        return phone;
-    }
+  // returning the untouched value when it contains non-digit chars or when it has a length != 10
+  if (digitsOnly.test(phone) === false || phone.length !== 10) {
+    return phone;
+  }
 
-    // returning the masked value
-    const countryCode = '234';
-    const prefix = phone.substring(0, 2);
-    const midfix = phone.substring(2, 6);
-    const sufix = phone.substring(6, 10);
+  // returning the masked value
+  const countryCode = '234';
+  const prefix = phone.substring(0, 2);
+  const midfix = phone.substring(2, 6);
+  const sufix = phone.substring(6, 10);
 
-    return `(${countryCode}) ${prefix}-${midfix}-${sufix}`;
+  return `(${countryCode}) ${prefix}-${midfix}-${sufix}`;
 }
 
-// export default maskNIGPhone;
+export default maskNIGPhone;

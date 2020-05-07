27e7037e9a8d1993bca9216af21f0a4e63d59c8d
Michael Olatunji <tundexmike@gmail.com>
2020-05-07 13:41:46 +0100

    make nigeria number function and adding unit tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-const sayHello = (name) => {
-    return `Hello ${name}`
+// regex that checks if value contains digits only
+const digitsOnly = /^\d+$/;
+
+// function to mask digits into US phone format
+function maskNIGPhone(phone) {
+  // returning null when getting null, undefined, or an object that is no string as a parameter
+  if (!phone || typeof phone !== 'string') {
+    return null;
+  }
+
+  // returning the untouched value when it contains non-digit chars or when it has a length != 10
+  if (digitsOnly.test(phone) === false || phone.length !== 10) {
+    return phone;
+  }
+
+  // returning the masked value
+  const countryCode = '234';
+  const prefix = phone.substring(0, 2);
+  const midfix = phone.substring(2, 6);
+  const sufix = phone.substring(6, 10);
+  
+  return `(${countryCode}) ${prefix}-${midfix}-${sufix}`;
 }
 
-const message = sayHello('Michael');
-
-console.log(message);
+export default maskNIGPhone;
--- test/index.js
+++ test/index.js
@@ -1,9 +1,28 @@
 import assert from 'assert';
+import maskNIGPhone from '../src/index';
+
+const testSamples = [
+    { input: 'abc', expectedResult: 'abc', description: 'should return pristine value when receiving "abc"' },
+    { input: 'abc1234567', expectedResult: 'abc1234567', description: 'should return pristine value when receiving "abc1234567"' },
+    { input: 'abcdefghij', expectedResult: 'abcdefghij', description: 'should return pristine value when receiving "abcdefghij"' },
+    { input: '9012345678', expectedResult: '(234) 90-1234-5678', description: 'should return (234) 90-1234-5678' },
+    { input: '8198765432', expectedResult: '(234) 81-9876-5432', description: 'should return (234) 81-9876-5432' },
+];
+
+
+// describe('Array', () => {
+//     describe('#indexOf', () => {
+//         it('should return -1 when the value is not present', () => {
+//             assert.equal([1,2,3].indexOf(4), -1)
+//         })
+//     })
+// })
 
 describe('Array', () => {
-    describe('#indexOf', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal([1,2,3].indexOf(4), -1)
+    testSamples.forEach(sample => {
+        it(sample.description, () => {
+            console.log(maskNIGPhone(sample.input))
+            assert.equal(maskNIGPhone(sample.input), sample.expectedResult)
         })
     })
 })
